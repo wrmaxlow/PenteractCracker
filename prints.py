20b9e1569e4e80dfc39d74a72c0f84426ace334c
wrmaxlow <111193998+wrmaxlow@users.noreply.github.com>
2022-08-14 15:16:07 -0400

    Create prints.py
    First version made in repl.it

---
+++ prints.py
@@ -0,0 +1,42 @@
+def space():
+	print()
+	print()
+
+def beginning():
+	print(" _____  _______ __   _ _______ _______  ______ _______ _______ _______")
+	print("|_____] |______ | \  |    |    |______ |_____/ |_____| |          |   ")
+	print("|       |______ |  \_|    |    |______ |    \_ |     | |_____     |   ")
+	print()
+	print("        _______  ______ _______ _______ _     _ _______  ______       ")
+	print("        |       |_____/ |_____| |       |____/  |______ |_____/       ")
+	print("        |_____  |    \_ |     | |_____  |    \_ |______ |    \_       ")
+	print()	
+	print()	
+	print()	
+	print("----------------------------------------------------------------------")
+	print("|                Please Input Your Encrypted Message                 |")
+	print("|(Note this program is unable to test for combos of ciphers and keys)|")
+	print("----------------------------------------------------------------------")
+	print()
+	print()
+
+
+def optiontext():
+	print("----------------------------------------------------------------------")
+	print("|                  What solutions should be run?                     |")
+	print("| 1. Run all ciphers and keys                                        |")
+	print("| 2. Run all ciphers                                                 |")
+	print("| 3. Run all keys                                                    |")
+	print("----------------------------------------------------------------------")
+	print()
+	print()
+
+
+def ending():
+	print("----------------------------------------------------------------------")
+	print("|                The selection was run successfully!                 |")
+	print("|                 Check results.txt for all results.                 |")
+	print("|      If using English recognition, view narrowed_results.txt       |")
+	print("----------------------------------------------------------------------")
+	print()
+	print()
